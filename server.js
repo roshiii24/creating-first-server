327dfd1cb5a7d67c2d5df7d7d911f989facb9fe2
roshiii24 <rrsaggarwal@gmail.com>
2022-08-21 23:45:14 +0530

    created routes in  server

--- server.js
+++ server.js
@@ -2,11 +2,33 @@ const http = require("http");
 
 const port = 8081;
 
+const toDoList = ["Complete Node Byte", "Play Cricket"];
+
 http
     .createServer((req, res) => {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<h1>Hello, this is from my server</h1>")
-        res.end();
+        const { method, url } = req;
+        if (url === "/todos") {
+            if (method === "GET") {
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.write(toDoList.toString());
+            } else if (method === "POST") {
+                let body = "";
+                req
+                    .on("error", (err) => {
+                        console.error(err);
+                    }).on("data", (chunks) => {
+                        body += chunks;
+                    }).on("end", () => {
+                        body = JSON.parse(body);
+                        console.log(body);
+                    })
+            }else {
+                res.writeHead(501);
+            }
+        } else {
+            res.writeHead(404);
+        }
+        res.end(); 
     })
     .listen(port, () => {
         console.log("Listening on port " + port);
